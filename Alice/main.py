f6405ccdca46ae4dee806de746d50eda691caf20
S4YroNz <98101455+S4YroNz@users.noreply.github.com>
2023-04-28 20:34:02 +0400

    Alice updated

--- Alice/main.py
+++ Alice/main.py
@@ -1,6 +1,8 @@
 import random
 import requests
 import json
+import Levenshtein
+import base64
 
 
 class User:
@@ -13,6 +15,7 @@ class User:
         self.lastquiz_id = r.get('lastquiz_id', None)
         self.cur_question = r.get('cur_question', None)
         self.result = r.get('result', {})
+        self.downloaded_images = r.get('downloaded_images', [])
 
     def toDict(self):
         return {key: value for key, value in vars(self).items()}
@@ -48,6 +51,10 @@ def cutTooLongText(res):
 
 
 def handle_dialog(req: dict, res, user: User):
+    # if user.downloaded_images:
+    #     for img_id in user.downloaded_images:
+    #         delete_image(img_id)
+    #     user.downloaded_images = []
     if req['session']['new']:
         send_greetings(res)
         send_yesno_suggest(res)
@@ -68,8 +75,8 @@ def handle_dialog(req: dict, res, user: User):
         # user.preview_quiz = False
         if 'YANDEX.CONFIRM' in intents:
             start_quiz(user)
-            handle_quiz(req, res, user, False)
-            send_quiz_suggests(res, user)
+            handle_quiz(req, res, user, quizzes[user.quiz_id], False)
+            send_quiz_suggests(res, user, quizzes[user.quiz_id])
             return
         elif 'YANDEX.REJECT' in intents:
             exit_quiz(user)
@@ -80,18 +87,20 @@ def handle_dialog(req: dict, res, user: User):
 
     if user.in_quiz:
         quiz = quizzes[user.quiz_id]
-        if user.cur_question < len(quiz['questions']) - 1:
+        print(
+            f'BEFORE: {user.cur_question=} {quiz["questions"][user.cur_question]["title"]=}')
+        if user.cur_question < len(quiz['questions']):
             if 'STOP' in intents:
                 exit_quiz(user)
                 res['response']['text'] = random.choice(
                     phraseVariables['exit_from_quiz'])
                 send_idle_suggests(res)
                 return
-            if handle_quiz(req, res, user, 'REPEAT_QUESTION' not in intents):
-                send_quiz_suggests(res, user)
+            if handle_quiz(req, res, user, quiz, 'REPEAT_QUESTION' not in intents):
+                send_quiz_suggests(res, user, quiz)
                 return
-        elif user.cur_question == len(quiz['questions']) - 1:
-            send_quiz_result(req, res, user)
+        if user.cur_question == len(quiz['questions']):
+            send_quiz_result(req, res, user, quiz)
             return
 
     if 'YANDEX.HELP' in intents:
@@ -99,7 +108,7 @@ def handle_dialog(req: dict, res, user: User):
 можешь выбрать один вариант из нескольких предложенных. Ответив на каждый вопрос, ты узнаешь результат"
         user.preview_quiz = False
         if user.in_quiz:
-            send_quiz_suggests(res, user)
+            send_quiz_suggests(res, user, quizzes[user.quiz_id])
         else:
             send_idle_suggests(res)
         return
@@ -115,13 +124,14 @@ def handle_dialog(req: dict, res, user: User):
                 return
         if 'START_QUIZ' in intents:
             user.preview_quiz = False
-            quiz = intents['START_QUIZ']['slots']['quiz_title']['value']
-            res['response']['text'] = f'Вы захотели запустить викторину {quiz}'
-            send_idle_suggests(res)
+            finded_quizz = find_quiz(
+                intents['START_QUIZ']['slots']['quiz_title']['value'], [i['title'] for i in quizzes])[0]
+            id_ = [i['id'] for i in quizzes if i['title'] == finded_quizz][0]
+            preview_quiz(user, res, id_)
             return
         if 'SHOW_TOP' in intents:
             user.preview_quiz = False
-            res['response']['text'] = 'Вывела топ'
+            send_top(res)
             send_idle_suggests(res)
             return
         if 'START_RANDOM_QUIZ' in intents:
@@ -143,13 +153,13 @@ def handle_dialog(req: dict, res, user: User):
             send_idle_suggests(res)
             return
 
+        if 'STOP' in intents:
+            res['response']['text'] = 'До встречи в следующий раз!'
+            res['response']['end_session'] = True
+            return
+
     if user.just_finished and 'REPEAT_QUIZ' in intents:
         preview_quiz(user, res, user.lastquiz_id)
-        return
-
-    if 'STOP' in intents:
-        res['response']['text'] = 'До встречи в следующий раз!'
-        res['response']['end_session'] = True
         return
 
     user.just_finished = False
@@ -162,30 +172,102 @@ def handle_dialog(req: dict, res, user: User):
         if user.cur_question == -1:
             send_yesno_suggest(res)
         else:
-            send_quiz_suggests(res, user)
+            send_quiz_suggests(res, user, quizzes[user.quiz_id])
     else:
         send_idle_suggests(res)
 
 
-def handle_quiz(req, res, user: User, next_=True):
+def send_top(res):
+    top = list(sorted(quizzes, key=lambda x: -x["views"]))[:5]
+    res['response']['text'] = f'''{top[0]["title"]}\n
+                    {top[1]["title"]}\n
+                    {top[2]["title"]}\n
+                    {top[3]["title"]}\n
+                    {top[4]["title"]}\n'''
+    res['response']['card'] = {
+        'type': 'ItemsList',
+        'header':
+            {
+                'text': 'Топ викторин'
+            },
+        'items': [{
+            'title': f'{i}) {top_title["title"]}',
+            'button':
+            {
+                'text': f'Запусти викторину {top_title["title"]}'
+            }
+        } for i, top_title in enumerate(top, start=1)]
+    }
+
+
+def find_quiz(user_req, all_titles, delta_distance=6):
+    symb = '.,/!?@"№;:<>#$%^&*()\|=+-_1234567890'
+    user_req = str(user_req)
+    for let in symb:
+        user_req = user_req.replace(let, '')
+    user_req = user_req.lower()
+
+    re = []
+    for i in range(len(all_titles)):
+        el = all_titles[i]
+        for let in symb:
+            el = el.replace(let, '')
+        el = el.lower()
+        re.append([all_titles[i], Levenshtein.distance(user_req, el)])
+
+    re = list(sorted(re, key=lambda x: x[1]))
+    ans = []
+    ans.append(re[0][0])
+    min_distance = re[0][1]
+    try:
+        if re[1][1] - min_distance < delta_distance:
+            ans.append(re[1][0])
+        if re[2][1] - min_distance < delta_distance:
+            ans.append(re[2][0])
+    except Exception:
+        pass
+    return ans
+
+
+def send_answers(res, title, answers):
+    card = {
+        'type': 'ItemsList',
+        'header':
+        {
+            'text': title
+        },
+        'items': [
+            {
+
+                'title': f'{i}) {answer_title}'
+            }
+            for i, answer_title in enumerate(answers, start=1)]
+    }
+    res['response']['card'] = card
+    text = '.'.join(answers)
+    res['response']['tts'] = text
+    res['response']['text'] = text
+
+
+def handle_quiz(req, res, user: User, quiz, next_=True):
     # Получение ответа
     if user.cur_question != -1 and next_:
-        if not check_quizAnswer(req, res, user):
-            return False
+        if not check_quiz_answer(req, res, user, quiz):
+            return True
 
     # Следующий вопрос
     user.cur_question += 1 if next_ else 0
-    question = quizzes[user.quiz_id]['questions'][user.cur_question]
+    if user.cur_question == len(quiz['questions']):
+        return False
+    question = quiz['questions'][user.cur_question]
     title = question['title']
     answers = [i['title'] for i in question['answers']]
-    res['response']['text'] = title + "\n" + \
-        "\n".join(f"{i}. {row}" for i, row in enumerate(answers, start=1))
+    send_answers(res, title, answers)
     return True
 
 
-def check_quizAnswer(req, res, user: User):
+def check_quiz_answer(req, res, user: User, quiz):
     intents = req['request']['nlu']['intents']
-    quiz = quizzes[user.quiz_id]
     question = quiz['questions'][user.cur_question]
     answers = question['answers']
     if not ('CHOOSE_ANSWER_2' in intents or 'CHOOSE_ANSWER_1' in intents):
@@ -199,8 +281,7 @@ def check_quizAnswer(req, res, user: User):
 
     # Начисление очков
     if quiz['type'] == 'person':
-        for character in answers[user_answer - 1]['characters']:
-            user.result[character] += 1
+        user.result[answers[user_answer - 1]['character']] += 1
     elif quiz['type'] == 'percent':
         correct = [i for i, a in enumerate(
             question['answers'], start=1) if a['is_true']][0]
@@ -209,27 +290,25 @@ def check_quizAnswer(req, res, user: User):
     return True
 
 
-def send_quiz_result(req, res, user: User):
-    # Проверка последнего ответа
-    check_quizAnswer(req, res, user)
-
-    # Вывод результата
-    quiz = quizzes[user.quiz_id]
+def send_quiz_result(req, res, user: User, quiz):
     if quiz['type'] == 'person':
         result = max(user.result.items(), key=lambda x: x[1])[0]
-        text = f'Ты - {result}'
-        res['response']['text'] = text
         character = [i for i in quiz['characters'] if i['name'] == result][0]
+        text = f'Ты - {result}. {character["description"]}'
+        res['response']['text'] = text
         if character['image']:
             res['response']['card'] = {}
             card = res['response']['card']
             card['type'] = 'BigImage'
             card['title'] = text
             card['description'] = character['description']
+            # character_image_id = upload_image(base64.decodebytes(
+            #     character['image'].encode('utf-8')))['id']
+            # user.downloaded_images.append(character_image_id)
+            # card['image_id'] = character_image_id
             card['image_id'] = character['image']
     elif quiz['type'] == 'percent':
-        result = round(
-            user.result / len(quizzes[user.quiz_id]['questions']) * 100)
+        result = round(user.result / len(quiz['questions']) * 100)
         res['response']['text'] = f'Ваш результат - {result}%. Вы можете пройти заново эту викторину или начать другую'
 
     send_idle_suggests(res)
@@ -250,23 +329,32 @@ def send_quiz_result(req, res, user: User):
     return
 
 
+# def get_quiz(id_):
+#     return requests.get(f'https://yaquizserver.onrender.com/api/quiz/{id_}').json()
+
+
 def preview_quiz(user: User, res, quiz_id):
     user.quiz_id = quiz_id
     user.preview_quiz = True
 
     # Превью
-    title = quizzes[user.quiz_id]['title']
-    description = quizzes[user.quiz_id]['description']
-    creator = quizzes[user.quiz_id]['creator']
-    preview = quizzes[user.quiz_id]['image']
-    res['response']['text'] = f"{title}\n{description}\nСоздатель:{creator}\nНачинаем?"
-    if quizzes[user.quiz_id]['image']:
+    quiz = quizzes[user.quiz_id]
+    title = quiz['title']
+    description = quiz['description']
+    creator = quiz['creator']
+    preview = quiz['image']
+    res['response']['text'] = f"{title}\n{description}\nСоздатель: {creator}. \nНачинаем?"
+    if quiz['image']:
         res['response']['card'] = {}
         card = res['response']['card']
         card['type'] = 'BigImage'
+#         title_image_id = upload_image(base64.decodebytes(\
+# preview.encode('utf-8')))['image']['id']
+#         user.downloaded_images.append(title_image_id)
+#         card['image_id'] = title_image_id
         card['image_id'] = preview
         card['title'] = f'{title}'
-        card['description'] = f'{description}\nСоздатель: {creator}\nНачинаем?'
+        card['description'] = f'{description}\nСоздатель: {creator}. \nНачинаем?'
     send_yesno_suggest(res)
 
 
@@ -275,9 +363,9 @@ def start_quiz(user: User):
     user.cur_question = 0
     quiz = quizzes[user.quiz_id]
     if quiz['type'] == 'person':
-
-        user.result = {character['name']
-            : 0 for character in quiz['characters']}
+        user.result = {
+            character['name']: 0 for character in quiz['characters']
+        }
     elif quiz['type'] == 'percent':
         user.result = 0
 
@@ -316,8 +404,8 @@ def send_idle_suggests(res):
     ]
 
 
-def send_quiz_suggests(res, user: User):
-    answers = len(quizzes[user.quiz_id]['questions']
+def send_quiz_suggests(res, user: User, quiz):
+    answers = len(quiz['questions']
                   [user.cur_question]['answers'])
     res['response']['buttons'] = [
         *[
@@ -341,8 +429,8 @@ def send_quiz_suggests(res, user: User):
 def send_creation_suggest(res):
     res['response']['buttons'] = res['response'].get('buttons', []) + [
         {
-            "title": "Создать",
-            "url": "https://youtube.com",
+            "title": "Создать викторину",
+            "url": "https://yaquizserver.onrender.com/",
             "hide": False
         }
     ]
@@ -371,7 +459,8 @@ def send_in_quiz_error(res):
 
 
 def send_error(res):
-    res['response']['text'] = "Извини, я не поняла, повтори пожалуйста"
+    res['response']['text'] = random.choice(
+        phraseVariables['error'])
 
 
 def upload_image(image_bits):
